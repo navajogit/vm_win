d5afc418c96c6549f815f0f9f35cfa38ba1ab0ba
navajogit <146178899+navajogit@users.noreply.github.com>
2023-09-27 14:46:53 +0200

    Update startup.ps1

--- startup.ps1
+++ startup.ps1
@@ -1,49 +1,46 @@
-Win vm faster setup script
+# Define the files to be downloaded with their URLs and descriptions
+$filesToDownload = @(
+    @{
+        Url = "https://updates.safing.io/latest/windows_amd64/packages/portmaster-installer.exe"
+        Description = "portmaster-installer.exe"
+    },
+    @{
+        Url = "https://windscribe.com/install/desktop/windows/Windscribe_2.6.14.exe"
+        Description = "Windscribe_2.6.14.exe"
+    },
+    @{
+        Url = "https://laptop-updates.brave.com/latest/winx64/BraveBrowserSetup.exe"
+        Description = "BraveBrowserSetup.exe"
+    },
+    @{
+        Url = "https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe"
+        Description = "spice-guest-tools-latest.exe"
+    },
+    @{
+        Url = "https://www.spice-space.org/download/windows/spice-webdavd/spice-webdavd-x64-latest.msi"
+        Description = "spice-webdavd-x64-latest.msi"
+    }
+)
 
-# Function to download a file and check for success
-function Download-File($url, $outputPath, $description) {
-    Write-Host "Do you want to download $description? (Y/N)"
+# Loop through the list of files and ask the user whether to download each one
+foreach ($fileInfo in $filesToDownload) {
+    Write-Host "Do you want to download $($fileInfo.Description)? (Y/N)"
     $downloadChoice = Read-Host
     if ($downloadChoice -eq "Y" -or $downloadChoice -eq "y") {
-        Write-Host "Downloading $description..."
-        Invoke-WebRequest -Uri $url -OutFile $outputPath
+        Write-Host "Downloading $($fileInfo.Description)..."
+        $outputPath = "$env:USERPROFILE\Downloads\$($fileInfo.Description)"
+        Invoke-WebRequest -Uri $fileInfo.Url -OutFile $outputPath
         if ($?) {
-            Write-Host "$description downloaded successfully."
+            Write-Host "$($fileInfo.Description) downloaded successfully."
         } else {
-            Write-Host "Failed to download $description. Please try downloading it manually or run the script again."
+            Write-Host "Failed to download $($fileInfo.Description). Please try downloading it manually or run the script again."
             exit
         }
     } else {
-        Write-Host "Skipping $description download."
+        Write-Host "Skipping $($fileInfo.Description) download."
     }
 }
 
-# Download and install portmaster-installer.exe
-Download-File -url "https://updates.safing.io/latest/windows_amd64/packages/portmaster-installer.exe" -outputPath "$env:USERPROFILE\Downloads\portmaster-installer.exe" -description "portmaster-installer.exe"
-
-# Add a delay between downloads
-Start-Sleep -Seconds 2
-
-# Download and install Windscribe_2.6.14.exe
-Download-File -url "https://windscribe.com/install/desktop/windows/Windscribe_2.6.14.exe" -outputPath "$env:USERPROFILE\Downloads\Windscribe_2.6.14.exe" -description "Windscribe_2.6.14.exe"
-
-# Add a delay between downloads
-Start-Sleep -Seconds 2
-
-# Download and install BraveBrowserSetup.exe
-Download-File -url "https://laptop-updates.brave.com/latest/winx64/BraveBrowserSetup.exe" -outputPath "$env:USERPROFILE\Downloads\BraveBrowserSetup.exe" -description "BraveBrowserSetup.exe"
-
-# Add a delay between downloads
-Start-Sleep -Seconds 2
-
-# Download and install Spice Guest Tools
-Download-File -url "https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe" -outputPath "$env:USERPROFILE\Downloads\spice-guest-tools-latest.exe" -description "spice-guest-tools-latest.exe"
-
-# Add a delay between downloads
-Start-Sleep -Seconds 2
-
-# Download and install Spice WebDAVd
-Download-File -url "https://www.spice-space.org/download/windows/spice-webdavd/spice-webdavd-x64-latest.msi" -outputPath "$env:USERPROFILE\Downloads\spice-webdavd-x64-latest.msi" -description "spice-webdavd-x64-latest.msi"
 
 
 # tweaks
